4ed88fc327f637802543d0b6bb9b351b50d0d1af
Wishk6 <sauringuillaume@gmail.com>
2022-03-25 00:19:46 +0100

    Animation/Responsive V2 + Image opti

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,68 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+/* <template>
+<quizz-component >
+<!-- Loader btnStart / click on mobile -->
+</quizz-component>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Options({
+import { defineComponent } from 'vue'
+
+import QuizzComponent from './components/quizzcomponent.vue'
+
+export default defineComponent({
+  name: 'App',
+
   components: {
-    HelloWorld,
+    QuizzComponent
   },
 })
-export default class App extends Vue {}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+ * {
+    font-size:28px;
+    margin: 0;
+    padding: 0;
+    letter-spacing: 1px;
+}
+
+a {
+  text-decoration: none;
+  font-weight: bold;
+  font-family: 'Roboto';
+  color : var(--globalColor)
+}
+
+html {
+  font-family: 'Roboto Slab', serif;
+  background: url(assets/mainBg.jpg) no-repeat center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  color:var(--globalColor);
+}
+
+:root {
+--globalColor : #ffffff;
+--shadow : #00000040;
+--globalBg1: #452a7a;
+--globalBg2: #713f81;
+--linear : linear-gradient(-80deg, var(--globalBg1), var(--globalBg2));
+}
+
+
+@media screen and (max-width:800px) {
+  * {
+    font-size: 4vw; 
+  }
 }
 </style>
+ */
---
+++ src/components/quizzcomponent.vue
@@ -0,0 +1,238 @@
+<!-- Template !-->
+<template>
+
+<div class="main">
+
+    <div class="headerWrapper">
+
+      <div class="questNbr" v-if="questions.length > 0 && questionNumber <= 20">
+          {{ questionNumber }}/20
+      </div>
+
+      <a href="https://github.com/Wishk6/pouroucontre" target="_blanck">
+        <div class="githubLogoCircle">
+          <img src="../assets/githubLogo.png" alt="Logo Github" title="Github Project" class="githubLogo">
+        </div>
+      </a>
+
+    </div>
+
+    <div class="titleWrapper" v-if="questions.length <= 0">
+
+        <div class="title">POUR</div>
+        <div class="title">OU</div>
+        <div class="title">CONTRE</div>
+
+    </div>
+
+    <div class="subTitleWrapper" v-if="questions.length <= 0">
+        <div class="subTitle">Êtes vous en accord avec la majorité ?</div>
+    </div>
+
+    <div v-if="questions.length <= 0" class="startButtonWrapper">
+
+        <div  v-on:click="getQuestions()" class="startButton">
+            <div class="startText">
+                COMMENCER
+            </div>
+        </div>
+
+    </div>
+
+        <div v-if="questions.length > 0 && questionNumber <= 20" class="secondDiv">
+
+            <div class="questionWrapper">
+
+              <div class="question">
+                {{ question.content }}
+              </div>
+
+            </div>
+
+            <div class="choiceWrapper">
+
+                <div v-on:click=" storeData(question.id, question.yes + 1, question.no, true)"
+                v-bind:class=" { forAgainstButton: boolAnswer == 0, forAgainstButtonDisabled: boolAnswer == 1, forAgainstButtonClicked : boolAnswer == 2 } ">
+
+                    <div v-if="boolAnswer == 2">
+                        {{
+                        Math.round(((question.yes + 1) / (question.yes + question.no + 1)) * 100)
+                        }}%
+                    </div>
+
+                    <div style="font-weight: bold;">POUR</div>
+
+                    <div v-if="boolAnswer == 2">
+                      {{ question.yes + 1 }} Votes
+                    </div>
+
+                </div>
+
+                <div :disabled="boolAnswer == 2" v-on:click=" storeData(question.id, question.yes, question.no + 1, false)"
+                v-bind:class="{forAgainstButton: boolAnswer == 0, forAgainstButtonDisabled : boolAnswer == 2, forAgainstButtonClicked : boolAnswer == 1 }">
+
+                    <div v-if="boolAnswer == 1">
+                      {{
+                        Math.round(((question.no + 1) / (question.yes + question.no + 1)) * 100)
+                      }}%
+                    </div>
+
+                    <div style="font-weight: bold;">CONTRE</div>
+
+                    <div v-if="boolAnswer == 1">
+                      {{ question.no + 1}} Votes
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+            <div  v-if="questions.length > 0 && questionNumber < 20 && !end" v-on:click="getRandomQuestion()">
+                <div v-bind:class="{ 'nextButtonHidden' : !answered, 'nextButton' : answered }">
+                  QUESTION SUIVANTE
+                </div>
+            </div>
+
+            <div class="finalDivWrapper">
+
+                <div v-if="end && !showStats" v-on:click="updateData()" class="resultButton">Voir les résultats</div>
+
+                <div v-if="showStats">
+                    Sur vos 20 réponses, {{ medium }} sont en accord avec la majorité,
+                    soit {{ Math.round((medium / 20) * 100) }}%
+                </div>
+        
+                <a v-if="showStats" class="resultButton" href="https://pouroucontre.cbym5024.odns.fr">
+                  MENU
+                </a>
+
+            </div>
+
+    </div>
+
+</template>
+
+<!-- Script !-->
+<script lang='ts'>
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'QuizzComponent',
+  data () {
+    return {
+      questions: [] /* all questions */,
+      answers: [{ id: 0, yes: 0, no: 0 }], // users's answers */
+      end: false /* checks if it the end of the questionary */,
+      questionNumber: 0 /* question nbr */,
+      question: '' /* actual question on the screen */,
+      medium: 0 /* number of answers equals to average answers */,
+      showStats: false /* checks if user want to see stats */,
+      boolAnswer: 0 /*  toggle css ForAgainstBtn, 1: against, 2: for  (check if better way to do this) */,
+      answered : false  /* check if answered actual quest */ 
+    }
+  },
+  methods: {
+
+    getQuestions: async function () {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+      }
+      fetch('https://pouroucontre.cbym5024.odns.fr/api/question', requestOptions)
+        .then(async (response) => {
+          const data = await response.json()
+          this.questions = data
+          this.getRandomQuestion()
+          if (!response.ok) {
+            const error = (data && data.message) || response.status
+            return Promise.reject(error)
+          }
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la requête', error)
+        })
+    },
+
+    getRandomQuestion: function () {
+      const actualRandInt = Math.floor(Math.random() * this.questions.length)
+      this.questionNumber++
+      this.answered = false
+      this.boolAnswer = 0
+      this.question = this.questions[actualRandInt]
+
+      this.questions = this.questions.filter((actualQuest) => {
+        return (
+          actualQuest !== this.questions[actualRandInt]
+        ) /* remove actual question from questions obj */
+      })
+    },
+
+    storeData: function (id: number, yes: number, no: number, choice: boolean) {
+      this.answered = true
+      if (this.questionNumber === 20) {
+        this.end = true
+      }
+      /* store data in array to analyse it */
+      if (this.boolAnswer === 0) {
+        if (!choice) {
+          const averageAnswer = no - 1 - yes >= 0
+          this.boolAnswer = 1
+
+          if (averageAnswer) { // if response same as average answer
+            this.medium++
+          }
+        } else {
+          const averageAnswer = yes - 1 - no >= 0
+          this.boolAnswer = 2
+
+          if (averageAnswer) {
+            this.medium++
+          }
+        }
+        const arr = { id: id, yes: yes, no: no }
+        this.answers.push(arr)
+      }
+    },
+
+    updateData: async function () {
+      /* update data in Db */
+      this.showStats = true
+      this.answers.forEach(
+        (element: { id: number; yes: number; no: number }) => {
+          const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              where: { id: element.id },
+              update: {
+                yes: element.yes,
+                no: element.no
+              }
+            })
+          }
+          fetch('https://pouroucontre.cbym5024.odns.fr/api/question/update', requestOptions)
+            .then(async (response) => {
+              const data = await response.json()
+              if (!response.ok) {
+                const error = (data && data.message) || response.status
+                return Promise.reject(error)
+              }
+            })
+            .catch((error) => {
+              console.error(
+                'Erreur durant la mise à jour de la base de données',
+                error
+              )
+            })
+        })
+    }
+  } // close methods
+})
+</script>
+<!-- Style !-->
+<style scoped>
+ @import 'quizzcomponent.css';
+</style>
